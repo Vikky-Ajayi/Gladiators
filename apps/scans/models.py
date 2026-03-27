2d08eef63216e1557f7bb13dd5202a79a1a0a835
unknown <vikkyoluwabukunmi@gmail.com>
2026-03-27 20:46:42 +0100

    Added models fields

--- apps/scans/models.py
+++ apps/scans/models.py
@@ -19,6 +19,9 @@ class FloodRiskZone(models.Model):
     max_lng   = models.DecimalField(max_digits=11, decimal_places=8)
     data_source = models.CharField(max_length=100, blank=True)
     notes     = models.TextField(blank=True)
+    flood_type = models.CharField(max_length=100, null=True, blank=True)
+    peak_months = models.CharField(max_length=200, null=True, blank=True)
+    last_major_flood_year = models.IntegerField(null=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True)
 
     class Meta:
